f577703399f37f972a4528aeb2411975f1a762d2
altunbas-huseyin <altunbas.huseyin@gmail.com>
2013-07-18 13:49:13 +0300

    Runtime Generate Button List
    runtim da butonlar olşturur ve flowlayoutpanel'e ekler.

---
+++ RuntimeButton/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RuntimeButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Button btn;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+      
+        private void flowLayoutPanel1_ControlAdded(object sender, ControlEventArgs e)
+        {
+            
+            
+            e.Control.Anchor = ((System.Windows.Forms.AnchorStyles)
+                (((System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.AutoScroll = false;  
+            for (int i = 0; i < 100; i++)
+            {   btn = new Button();
+                btn.Margin = new Padding(1, 1, 1, 1);
+                btn.Name = "BTN";
+                btn.Text = "BTN";
+                btn.Left = -15;
+
+                this.flowLayoutPanel1.Controls.Add(btn);
+                
+            }
+        }
+
+    }
+}
